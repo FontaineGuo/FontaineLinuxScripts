a39d59a4c5e0178fcd2fd6638fa24f2a19dab711
Fontaine <fontaine_gzy@163.com>
2018-05-04 11:42:36 +0800

    delete single bash

--- Software/Install_python.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install python-dev python-pip python3-dev python3-pip
-sudo -H pip3 install -U pip numpy
-pip install numpy scipy matplotlib scikit-image scikit-learn ipython
