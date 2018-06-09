8a92d6278a0327058f7213413bcf5b33c6a623d4
Fontaine_PC_DEEPIN <fontaine_gzy@163.com>
2018-06-09 12:49:40 +0800

    add virtulenv config

--- Software/Install_InitSoft.sh
+++ Software/Install_InitSoft.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 
 # 1. install cmake-qt-gui
 echo "Install cmake-qt-gui"
-sudo apt-get install cmake-qt-gui
+sudo apt-get install cmake cmake-qt-gui
 
 # 2. install cmake
 echo "Install tree"
@@ -14,7 +14,8 @@ echo "Install python"
 sudo apt-get update
 sudo apt-get install python-dev python-pip python3-dev python3-pip
 sudo -H pip3 install -U pip numpy
-sudo pip3 install virtualenvwrapper virtulenv numpy scipy matplotlib scikit-image scikit-learn ipython
+sudo -H pip2 install -U pip numpy
+sudo pip3 install numpy scipy matplotlib scikit-image scikit-learn ipython
 
 # 4. install tsocks
 echo "Install tsocks"
@@ -23,6 +24,19 @@ sudo apt-get install tsocks
 # 5. install virtualenvwrapper virtulenv
 echo "install virtualenvwrapper virtulenv"
 sudo pip3 install virtualenvwrapper virtulenv
+
+# write the following to the /.bashrc
+######################################
+# "# Virtual Environment Wrapper" 
+# export WORKON_HOME=$HOME/.virtualenvs
+# export PROJECT_HOME=$HOME/Devel
+# source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+######################################
+echo "# Virtual Environment Wrapper"  >> ~/.bashrc
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+echo "export PROJECT_HOME=$HOME/Devel" >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc
 
 
 # 6. software-properties-common
