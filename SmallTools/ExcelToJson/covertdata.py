fb5573104522cfc986b03819040024371a354eb5
Fontaine_470S_win10 <fontaine_gzy@163.com>
2018-09-21 00:24:20 +0800

    add some methods
    add get_key_name()

--- SmallTools/ExcelToJson/covertdata.py
+++ SmallTools/ExcelToJson/covertdata.py
@@ -2,11 +2,17 @@ import xlrd
 
 
 # get all the key from left to right
-
-# get the line by id
+def get_key_name(worksheet):
+    sh = worksheet.sheet_by_index(0)
+    print(sh.row_values(0))
 
 # get the key type
 
 # get the value and covert it by type
 
 # get the json key-pair
+
+worksheet = xlrd.open_workbook('test.xlsx')
+get_key_name(worksheet)
+
+
