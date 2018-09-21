972c78c1199463dd7bfa5b61212a612171bad4be
Fontaine_470S_win10 <fontaine_gzy@163.com>
2018-09-21 21:43:04 +0800

    covertdata
    implement some method and test

--- SmallTools/ExcelToJson/covertdata.py
+++ SmallTools/ExcelToJson/covertdata.py
@@ -4,15 +4,18 @@ import xlrd
 # get all the key from left to right
 def get_key_name(worksheet):
     sh = worksheet.sheet_by_index(0)
-    print(sh.row_values(0))
+    return sh.row_values(0)
 
 # get the key type
-
+def get_value_type(TypeNamePair):
+    print(TypeNamePair.split('-',1))
 # get the value and covert it by type
 
 # get the json key-pair
 
 worksheet = xlrd.open_workbook('test.xlsx')
-get_key_name(worksheet)
+key_list = get_key_name(worksheet)
+print(key_list)
+print(type(key_list[1]))
 
 
