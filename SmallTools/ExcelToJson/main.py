ad42434a52e59a592a89b74b40d974edc3cea5ba
Fontaine_470S_win10 <fontaine_gzy@163.com>
2018-09-27 21:57:59 +0800

    test main

---
+++ SmallTools/ExcelToJson/main.py
@@ -0,0 +1,19 @@
+import xlrd
+import json
+from collections import OrderedDict
+from covertdata import *
+from general import * 
+
+create_project_dir('test')
+
+worksheet = xlrd.open_workbook('test.xlsx')
+
+keyname_pair_list = get_key_name(worksheet)
+row_values_list = get_vaule(get_row_by_id(worksheet, 1))
+
+sheet_dic_list = []
+sheet_dic_list.append(convert_data_to_json(keyname_pair_list, row_values_list))
+
+j = json.dumps(sheet_dic_list)
+
+create_json('test', 'test', j)
