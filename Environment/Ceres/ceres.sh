ba6f8d46056ce04b3bc1b9fed4efe0201edbf822
Fontaine <fontaine_gzy@163.com>
2018-12-08 22:54:47 +0800

    ceres install script

--- Environment/Ceres/ceres.sh
+++ Environment/Ceres/ceres.sh
@@ -13,3 +13,21 @@ sudo apt-get install libsuitesparse-dev
 # sudo add-apt-repository ppa:bzindovic /suitesparse-bugfix-1319687
 # sudo apt-get update
 # sudo apt-get install libsuitesparse-dev
+
+git clone https://github.com/ceres-solver/ceres-solver.git
+
+cd ceres-solver
+git checkout 1.14.0
+mkdir build
+cd build
+
+# if you want to install the ceres in the default location
+cmake -DCMAKE_BUILD_TYPE=RELEASE ..
+make -j4
+sudo make install
+
+# if you want to install the ceres in the custom location
+# cmake -DCMAKE_BUILD_TYPE=RELEASE \
+#       -DCMAKE_INSTALL_PREFIX=/custompath ..
+# make -j4 
+# make install
