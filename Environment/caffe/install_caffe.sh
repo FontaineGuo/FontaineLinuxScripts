ffed82a3d018d810b4dae13160eec1bd8dc35466
Fontaine <fontaine_gzy@163.com>
2018-12-14 17:20:07 +0800

    caffe

---
+++ Environment/caffe/install_caffe.sh
@@ -0,0 +1,12 @@
+
+
+sudo apt-get install -y libopenblas-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libgoogle-glog-dev
+sudo apt-get install -y libgflags-dev
+sudo apt-get install -y libhdf5-dev
+
+sudo apt-get install -y liblmdb-dev
+sudo apt-get install -y libleveldb-dev
+sudo apt-get install -y libsnappy-dev
