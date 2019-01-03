70b25ff6e3fc444dfb0b7f6e0f82f9b1fa2cf44d
Fontaine_PC_Win10 <fontaine_gzy@163.com>
2019-01-03 23:05:16 +0800

    edit error process

--- SmallTools/PhotoDivider/PhotoDivider.py
+++ SmallTools/PhotoDivider/PhotoDivider.py
@@ -11,21 +11,24 @@ def getDate(filename):
         fd = Image.open(filename)
     except:
         raise "unopen file[%s]\\n" % filename 
-    data = fd._getexif()
-    if data:
-        #get time
-        try:
-            t = data['EXIF DateTimeOriginal']
-            #transfer time format into  yyyy-mm-dd 的格式
-            return str(t).replace(":","-")[:10]
-        except:
-            pass
-    #if not get exif ，use its created time as DateTimeOriginal
-    state = os.stat(filename)
-    return time.strftime("%Y-%m-%d", time.localtime(state[-2]))
+    try:
+        data = fd._getexif()
+        if data:
+            #get time
+            try:
+                t = data['EXIF DateTimeOriginal']
+                #transfer time format into  yyyy-mm-dd 的格式
+                return str(t).replace(":","-")[:10]
+            except:
+                pass
+        #if not get exif ，use its created time as DateTimeOriginal
+        state = os.stat(filename)
+        return time.strftime("%Y-%m-%d", time.localtime(state[-2]))
+    except:
+        return 0
 
 
-def showFileProperties(path):
+def showFileProperties(path, tempfolder):
     '''显示文件的属性。包括路径、大小、创建日期、最后修改时间，最后访问时间'''
     import time,os
     #go through every file of dir
@@ -40,18 +43,24 @@ def showFileProperties(path):
             if e.lower() not in ('.jpg','.png'):
                 continue   
             #get DateTimeOriginal
-            t = getDate( filename )[:7]
-            pwd = root + '\\' + t
-            dst = pwd + '\\' + os.path.basename(filename)
-            print("Move " + filename + "   to   " + dst )
-            if not os.path.exists(pwd):
-                 os.mkdir(pwd)
-                 shutil.copy2(filename, dst)
+            t = getDate( filename )
+            if t != 0:
+                t = t[:7]
+                pwd = root + '\\' + t
+                dst = pwd + '\\' + os.path.basename(filename)
+                print("Move " + filename + "   to   " + dst )
+                if not os.path.exists(pwd):
+                    os.mkdir(pwd)
+                    shutil.copy2(filename, dst)
+                else:
+                    shutil.copy2(filename, dst)
             else:
-                shutil.copy2(filename, dst)
-
+                shutil.copy2(filename, tempfolder)
 
 
 if __name__ == "__main__":
     path = "."
-    showFileProperties("D:\Photo\未整理")
+    tempfolder = '.\\未知时间'
+    if not os.path.exists(tempfolder):
+                os.mkdir(tempfolder)
+    showFileProperties(".\\", tempfolder)
